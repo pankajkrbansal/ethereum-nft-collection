ae91b5545fd78bf726225590f8b47717a8e601d3
pankajkrbansal <pankajbansal.pkb@gmail.com>
2023-02-08 00:00:21 +0530

    Added logic for generating random metadata

--- blockchain/contracts/EpicNFT.sol
+++ blockchain/contracts/EpicNFT.sol
@@ -4,38 +4,104 @@ pragma solidity >=0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
 import "@openzeppelin/contracts/utils/Counters.sol";
+import { Base64 } from "./libraries/Base64.sol";
 
 contract EpicNFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds; // using to keep track of count of NFT's
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] firstWords = ["Manga", "India", "Economy", "World", "Error", "Blockchain"];
+    string[] secondWords = ["Comics", "Spices", "West", "Earth", "Bugs", "NewTech"];
+    string[] thirdWords = ["Japan", "Heritage", "USA", "Home", "Code", "NewProblems"];
 
     constructor() ERC721("EpicNFT", "ENFT") {
         console.log("NFT Contract");
     }
 
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        // I seed the random generator. More on this in the lesson. 
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
     // function to create NFT on user action
     function createEpicNFT() public {
         uint256 nftId = _tokenIds.current();
+        
+        // We go and randomly grab one word from each of the three arrays.
+        string memory first = pickRandomFirstWord(nftId);
+        string memory second = pickRandomSecondWord(nftId);
+        string memory third = pickRandomThirdWord(nftId);
+        string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+        // I concatenate it all together, and then close the <text> and <svg> tags.
+        string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+        console.log("\n==============================\n");
+        console.log(finalSvg);
+        console.log("\n==============================\n");
+
+        // Get all the JSON metadata in place and base64 encode it.
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',Base64.encode(bytes(finalSvg)),// We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        '"}'
+                    )
+                )
+            )
+        );
+        
+        // Just like before, we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
         // using _safeMint for creating NFT for user
         _safeMint(msg.sender, nftId);
         // return nft metadata
-        tokenURI(nftId);
+        _setTokenURI(nftId, finalTokenUri);
+        // tokenURI(nftId);
         // increment counter
         _tokenIds.increment();
     }
 
-    // Set the NFT's metadata
-    function tokenURI(
-        uint256 _tokenId
-    ) public view override returns (string memory) {
-        require(_exists(_tokenId));
-        return
-            string(
-                abi.encodePacked(
-                    "data:application/json;base64,",
-                    "eyJuYW1lIjoiRXBpY0xvcmRIYW1idXJnZXIiLCJkZXNjcmlwdGlvbiI6IkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTB4dmNtUklZVzFpZFhKblpYSThMM1JsZUhRK0Nqd3ZjM1puUGc9PSJ9"
-                )
-            );
-    };
+    // // Set the NFT's metadata
+    // function tokenURI(
+    //     uint256 _tokenId
+    // ) public view override returns (string memory) {
+    //     require(_exists(_tokenId));
+    //     return
+    //         string(
+    //             abi.encodePacked(
+    //                 "data:application/json;base64,",
+    //                 "eyJuYW1lIjoiRXBpY0xvcmRIYW1idXJnZXIiLCJkZXNjcmlwdGlvbiI6IkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTB4dmNtUklZVzFpZFhKblpYSThMM1JsZUhRK0Nqd3ZjM1puUGc9PSJ9"
+    //            )
+    //         );
+    // };
 }
