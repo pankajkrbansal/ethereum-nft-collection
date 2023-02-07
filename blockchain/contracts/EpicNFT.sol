b6181cf233ba3363990ba06d90a0d35fa1db39cf
pankajkrbansal <pankajbansal.pkb@gmail.com>
2023-02-08 00:03:21 +0530

    Added logic for generating random metadata & correction for ERC721Storage

--- blockchain/contracts/EpicNFT.sol
+++ blockchain/contracts/EpicNFT.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import { Base64 } from "./libraries/Base64.sol";
 
-contract EpicNFT is ERC721 {
+contract EpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds; // using to keep track of count of NFT's
     string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
